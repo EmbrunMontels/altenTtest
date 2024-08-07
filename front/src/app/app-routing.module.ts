af8f1e0e179f03ba4e2e69da0419f6d73b462622
arcta <arcta09@gmail.com>
2024-08-07 18:13:48 +0200

    Creating products and admin/products route

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsAdminComponent} from "./src/app/product/products-admin/products-admin.component";
+import {ProductsComponent} from "./src/app/product/products/products.component";
 
 const routes: Routes = [
+  { path: 'admin/products', component: ProductsAdminComponent },
+  { path: 'products', component: ProductsComponent}
 
 ];
 
