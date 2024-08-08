8b159046960d63be806130b2f1650943b8ea41b7
arcta <arcta09@gmail.com>
2024-08-08 14:07:44 +0200

    create back (revert from previous commit)

---
+++ back/src/main/java/altenshop/test/altentest/AltentestApplication.java
@@ -0,0 +1,13 @@
+package altenshop.test.altentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltentestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltentestApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/altenshop/test/altentest/AltentestApplicationTests.java
@@ -0,0 +1,13 @@
+package altenshop.test.altentest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltentestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
